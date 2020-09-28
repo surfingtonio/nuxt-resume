5362d5b524be75b6a1e7d864c16ec6d1be9c1189
Danting, Marc <marc@danting.ca>
2020-09-28 16:50:07 -0700

    Cleaned-up animations.

--- layouts/default.vue
+++ layouts/default.vue
@@ -9,8 +9,36 @@
 <script lang="ts">
 import Vue from "vue";
 
+let timeout: number[] = [];
+
 export default Vue.extend({
   name: "Layout",
+  mounted() {
+    window.addEventListener("mousemove", function (ev) {
+      let show =
+        ev.clientY > window.innerHeight * 0.75 ||
+        ev.clientX > window.innerWidth * 0.75;
+      let fab = document.querySelectorAll(".action-button");
+
+      fab.forEach((e, i) => {
+        if (show) {
+          e.classList.toggle("action-button--show", true);
+          e.classList.toggle("action-button--hide", false);
+
+          if (timeout[i]) {
+            clearTimeout(timeout[i]);
+            delete timeout[i];
+          }
+        } else if (!timeout[i]) {
+          timeout[i] = window.setTimeout(() => {
+            e.classList.toggle("action-button--show", false);
+            e.classList.toggle("action-button--hide", true);
+            delete timeout[i];
+          }, 3000);
+        }
+      });
+    });
+  },
   data() {
     return {
       buttons: [
